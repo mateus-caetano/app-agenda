f91938ce05532cd1e710d5a76c7199a98cece1c5
mateus-caetano <mateus.caetano@alu.ufc.br>
2021-08-10 17:25:30 -0300

    Map visualization

--- agenda/app/src/main/java/com/mateus/agenda/MainActivity.kt
+++ agenda/app/src/main/java/com/mateus/agenda/MainActivity.kt
@@ -4,9 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.navigation.findNavController
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,4 +29,13 @@ class MainActivity : AppCompatActivity() {
         val dialog = NewEventDialog()
         dialog.show(supportFragmentManager, "newEvent")
     }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(LatLng(-3.4373045,-39.1439613))
+                .title("Marker")
+        )
+    }
+
 }
